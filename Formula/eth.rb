94fcf02b49142ddb7928a5970b39d3488407a6c6
Ayush Modi <ayushmodi2003@gmail.com>
2025-11-14 02:12:21 +0530

    chore: initial eth formula

--- Formula/eth.rb
+++ Formula/eth.rb
@@ -3,13 +3,15 @@ class Eth < Formula
 	homepage "https://github.com/ayushm2003/ethup"
 	version "0.1.0"
   
-	url "https://github.com/ayushm2003/ethup/releases/download/v0.1.0/eth-aarch64-macos.tar.gz"
-	sha256 "20f3efad04b6e286691184f6b0f845c68454773d099b73c3032e6724ec25c9d4"
-  
-	bottle :unneeded
+	on_macos do
+		on_arm do
+			url "https://github.com/ayushm2003/ethup/releases/download/v0.1.0/eth-aarch64-macos.tar.gz"
+			sha256 "20f3efad04b6e286691184f6b0f845c68454773d099b73c3032e6724ec25c9d4"
+		end
+	end
   
 	def install
-	  bin.install "eth"
+		bin.install "eth"
 	end
   end
   
