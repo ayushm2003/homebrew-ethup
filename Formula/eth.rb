0f125444f7b2fd40893587bab851185d91dce6ae
Ayush Modi <ayushmodi2003@gmail.com>
2025-11-14 01:26:57 +0530

    fix formula for mac

--- Formula/eth.rb
+++ Formula/eth.rb
@@ -3,8 +3,14 @@ class Eth < Formula
 	homepage "https://github.com/ayushm2003/ethup"
 	version "0.1.0"
   
-	url "https://github.com/ayushm2003/ethup/releases/download/v0.1.0/eth-aarch64-macos.tar.gz"
-	sha256 "20f3efad04b6e286691184f6b0f845c68454773d099b73c3032e6724ec25c9d4"
+	on_macos do
+	  if Hardware::CPU.arm?
+		url "https://github.com/ayushm2003/ethup/releases/download/v0.1.0/eth-aarch64-macos.tar.gz"
+		sha256 "20f3efad04b6e286691184f6b0f845c68454773d099b73c3032e6724ec25c9d4"
+	  else
+		odie "Intel macOS build not available yet."
+	  end
+	end
   
 	def install
 	  bin.install "eth"
