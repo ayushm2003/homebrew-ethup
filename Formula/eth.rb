df224e66f1d6e4f73de8efdcbf8ac7f3ce18c24a
Ayush Modi <ayushmodi2003@gmail.com>
2025-11-15 01:35:35 +0530

    v0.3.0

--- Formula/eth.rb
+++ Formula/eth.rb
@@ -1,12 +1,12 @@
 class Eth < Formula
 	desc "Simple Ethereum node orchestrator (reth + lighthouse)"
 	homepage "https://github.com/ayushm2003/ethup"
-	version "0.2.0"
+	version "0.3.0"
   
 	on_macos do
 		on_arm do
-			url "https://github.com/ayushm2003/ethup/releases/download/v0.2.0/eth-aarch64-macos.tar.gz"
-			sha256 "3741423e32411afbb8b3ba47a7b74252e9f4d4067423ae827a34dbef05f95afc"
+			url "https://github.com/ayushm2003/ethup/releases/download/v0.3.0/eth-aarch64-macos.tar.gz"
+			sha256 "398a0837f941cbd5e5db60db7d65d4db750b9678aefc844725880c796154e70b"
 		end
 	end
   
